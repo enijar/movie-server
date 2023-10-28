5d69901d51b38fc0f62cf4ab85aeabffe95b034c
Enijar <TheEnijar@gmail.com>
2023-10-28 19:39:51 +0100

    chore: handle fullscreen video

--- app/src/components/video/video.style.ts
+++ app/src/components/video/video.style.ts
@@ -1,10 +1,21 @@
 import styled from "styled-components";
+import Link from "next/link";
 
 export const Info = styled.div`
   display: grid;
   grid-template-columns: 1fr max-content;
   align-items: center;
   inline-size: 10rem;
+`;
+
+export const Back = styled(Link)`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+
+  svg {
+    inline-size: 1rem;
+  }
 `;
 
 export const Wrapper = styled.div`
@@ -17,6 +28,7 @@ export const Wrapper = styled.div`
     inline-size: 100%;
     aspect-ratio: 16 / 9;
     background-color: #333333;
+    cursor: pointer;
   }
 
   h1 {
--- app/src/components/video/video.tsx
+++ app/src/components/video/video.tsx
@@ -1,19 +1,44 @@
 "use client";
 
+import React from "react";
 import * as Style from "@/components/video/video.style";
-import { MovieType } from "@/types";
+import screenfull from "screenfull";
+import { type MovieType } from "@/types";
 import Rating from "@/components/rating/rating";
+import BackIcon from "@/icons/back-icon";
 
 type Props = MovieType & {
   posterUrl: string;
 };
 
 export default function Video(props: Props) {
+  const videoRef = React.useRef<HTMLVideoElement>(null);
+
   const [year] = props.year.split("-");
+
+  React.useEffect(() => {
+    const video = videoRef.current;
+    if (video === null) return;
+    if (!screenfull.isEnabled) return;
+
+    function fullscreen() {
+      if (video === null) return;
+      screenfull.request(video).catch((err) => console.error(err));
+    }
+
+    video.addEventListener("click", fullscreen);
+    return () => {
+      video.removeEventListener("click", fullscreen);
+    };
+  }, []);
 
   return (
     <Style.Wrapper>
-      <video poster={props.posterUrl} />
+      <Style.Back href="/movies">
+        <BackIcon />
+        <span>Back to movies</span>
+      </Style.Back>
+      <video ref={videoRef} poster={props.posterUrl} />
       <h1>{props.title}</h1>
       <Style.Info>
         <Rating rating={props.rating} />
